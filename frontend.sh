659f438941f2a814ad78bf45cde70bd7d463d7e8
poojitha <tenepalle.0212@gmail.com>
2023-11-22 03:48:13 -0800

    backend

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,10 @@
 dnf install nginx -y
+cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cp expense.conf /etc/systemd/system/backend.service
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl start nginx
 systemctl restart nginx
