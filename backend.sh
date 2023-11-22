28c8aec4b232923d96be324d049f797a245f10f6
poojitha <tenepalle.0212@gmail.com>
2023-11-22 03:37:09 -0800

    backend

---
+++ backend.sh
@@ -0,0 +1,15 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+cp backend.service /etc/systemd/system/backend.service
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+systemctl demon reload
+systemctl enable backend
+systemctl restart backend
+mysql -h <mysql-dev.nidhisolutions.online> -uroot -pExpenseApp@1 < /app/schema/backend.sql
